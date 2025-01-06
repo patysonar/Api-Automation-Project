92a9d2148edcb5372b3491c49e6aa6dfeb03598a
Patrícia Souza <patysonar@gmail.com>
2025-01-06 15:54:13 -0300

    Adicionando novos testes Atualização e Deleção do usuario

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,18 +1,34 @@
-
 describe('Registro de Usuário', () => {
-  let userId;
-  let userToken;
-  before(() => {
+  it('Registrando o usuário com sucesso', () => {
+    // Teste de registro
     cy.registerUser('eve.holt@reqres.in', 'pistola').then((response) => {
-      userId = response.body.id;
-      userToken = response.body.token;
+      const userId = response.body.id;
+      const userToken = response.body.token;
+
+      cy.log(
+        `Requisição realizada com sucesso! ID: ${userId}, Token: ${userToken}`,
+      );
     });
   });
 
-  it('Deve registrar o usuário com sucesso', () => {
-    // Teste de registro
-    cy.log(`Requisição realizada com sucesso! ID: ${userId}, Token: ${userToken}`);
+  it('Atualizando o usuário', () => {
+    // Teste de atualização com um usuário já registrado
+    const userId = 2; // Um ID fixo ou gerado dinamicamente
+    const updatedUserData = {
+      nome: 'morpheus',
+      emprego: 'zion resident',
+    };
+    cy.updateUser(userId, updatedUserData).then((response) => {
+      const updatedAt = response.body.updatedAt;
+      cy.log(`Informações do usuário atualizadas com sucesso. Novo nome: ${response.body.nome}, 
+        Novo emprego: ${response.body.emprego}, Data de atualização: ${updatedAt}`);
+    });
+  });
+
+  it('Deletando o usuário com sucesso', () => {
+    // Teste de deleção do usuário
+    let userId;
+    cy.deleteUser(userId); // Passando o ID do usuário para o comando de deleção
+    cy.log(`Usuário com ID ${userId} deletado com sucesso.`);
   });
 });
-
-  
