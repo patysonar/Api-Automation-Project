b3d0c88937338c3eda29bc5d2968c427402ca0f2
Patrícia Souza <patysonar@gmail.com>
2025-01-06 23:08:47 -0300

    Realizando adição de fluxos negativos  e adicionando paramentro do status

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,12 +1,22 @@
 describe('Registro de Usuário', () => {
   it('Registrando o usuário com sucesso', () => {
-    // Teste de registro
-    cy.registerUser('eve.holt@reqres.in', 'pistola').then((response) => {
+    // Teste de registro com status code esperado de 200
+    cy.registerUser('eve.holt@reqres.in', 'pistol', 200).then((response) => {
       const userId = response.body.id;
       const userToken = response.body.token;
 
       cy.log(
         `Requisição realizada com sucesso! ID: ${userId}, Token: ${userToken}`,
+      );
+    });
+  });
+
+  it('Tentando registrar um usuário com dados inválidos', () => {
+    // Teste de registro com status code esperado de 400 (erro)
+    cy.registerUser('sydney@fife', null, 400).then((response) => {
+      // Log da mensagem de erro retornada pela API
+      cy.log(
+        `Erro esperado no registro: Status ${response.status}, Mensagem: ${response.body.error}`,
       );
     });
   });
@@ -18,17 +28,34 @@ describe('Registro de Usuário', () => {
       nome: 'morpheus',
       emprego: 'zion resident',
     };
-    cy.updateUser(userId, updatedUserData).then((response) => {
+    cy.updateUser(userId, updatedUserData, 200).then((response) => {
       const updatedAt = response.body.updatedAt;
       cy.log(`Informações do usuário atualizadas com sucesso. Novo nome: ${response.body.nome}, 
         Novo emprego: ${response.body.emprego}, Data de atualização: ${updatedAt}`);
     });
   });
 
+  it('Tentando atualizar o usuário com dados inválidos', () => {
+    const userId = 2;
+    const updatedUserData = {
+      nome: '',
+      emprego: '',
+    };
+    cy.updateUser(userId, updatedUserData, 400).then((response) => {
+      cy.log(`Falha esperada na requisição: ${response.statusText}`);
+    });
+  });
+
   it('Deletando o usuário com sucesso', () => {
     // Teste de deleção do usuário
-    let userId;
+    let userId = 3;
     cy.deleteUser(userId); // Passando o ID do usuário para o comando de deleção
     cy.log(`Usuário com ID ${userId} deletado com sucesso.`);
+  });
+
+  it('Tentando deletar um usuário inexistente', () => {
+    let userId = 99999; // ID fictício
+    cy.deleteUser(userId, 404); // Espera-se erro 404 (não encontrado)
+    cy.log('Falha esperada ao tentar deletar usuário inexistente.');
   });
 });
